2c5d310e5442692a4b7e2ec5c2c59afbbef3db91
asilbeksodiqov <asilxonsodiqov@gmail.com>
2026-07-20 16:22:21 +0500

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,3 +1,3 @@
 // Apps Script'ni "Web App" sifatida deploy qilgandan so'ng olingan URL
 // (masalan: https://script.google.com/macros/s/AKfycb.../exec)
-const API_URL = 'https://script.google.com/macros/s/AKfycbzJhijL2IfkzX8mgZ6hz1wg4UbIWPyPG4IUylFpbLba3Y0fhXFBOvLvzgqQ-yvs3ovx/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbyq9TUixSuTxJCisfI5T9xu_-vXHwK5VayO8gVM2qaaH4_mo_nIYuu4e8yb73yid6lK/exec';
