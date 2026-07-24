e0bd98346b409d248d70c2b399ce45ad2b5e734a
asilbeksodiqov <asilxonsodiqov@gmail.com>
2026-07-24 11:43:18 +0500

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,3 +1,7 @@
 // Apps Script'ni "Web App" sifatida deploy qilgandan so'ng olingan URL
 // (masalan: https://script.google.com/macros/s/AKfycb.../exec)
-const API_URL = 'https://script.google.com/macros/s/AKfycbwYBEvlC2HfKXJDiIk3jMvWlza79WzX3W4EnOy-v8O8HG2BnVLIcsFdHFlHdQiRbf2_/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbxPLKqgUkkO216h6ei3RXG2omjGWFOkv8fNDKD4lBo9Zb3Xhi5WCZj37FejyOWIxxK-/exec';
+
+// Filial panelda hujjat yuborishda xatolik chiqsa, filial shu Telegram
+// akkauntga murojaat qilishi uchun ishlatiladi (branch.js).
+const TELEGRAM_HELP_USERNAME = 'Asilbek_Sodiqov';
